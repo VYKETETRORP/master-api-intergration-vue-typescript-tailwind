6c3c616054e47bc90a938d2084dd093580d43b6a
VYKETETRORP <lesleygs56@gamil.com>
2024-08-29 08:40:29 +0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
+
+
+// import path from 'node:path'
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// import tailwind from 'tailwindcss'
+// import autoprefixer from 'autoprefixer'
+
+// export default defineConfig({
+//   css: {
+//     postcss: {
+//       plugins: [tailwind(), autoprefixer()],
+//     },
+//   },
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+// })
