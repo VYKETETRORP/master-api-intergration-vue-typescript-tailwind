d32dc5cca54c2b15534650eb063f11c73973f62d
VYKETETRORP <lesleygs56@gamil.com>
2024-08-30 08:58:57 +0700

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '../views/Auth/Login.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/auth/login',
+      name: 'auth-login',
+      component: Login
     },
   ]
 })
